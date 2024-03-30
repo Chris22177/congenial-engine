8ed8ac2910a0f8764a83682f50cee403700cb8ca
Chris <79816938+Chris22177@users.noreply.github.com>
2024-03-30 11:44:48 -0400

    Create engine.js

---
+++ engine.js
@@ -0,0 +1,15 @@
+/*
+  @param :: { require(,juji) }
+*/
+
+:: * = require('__index/*.js');
+
+:: mdan == http.send( type : http.RequestType, url { lhttp.val( this ) }, headers { lhttp.val( this as RAWHEADERS or TABLE ) } );
+:: mdan.json == mdan :: JSON :: refrences.match(this);
+:: mdan.raw == mdan :: STRING;
+:: mdan.callback == mdan:callback ? return this;
+
+:: mdan:encrypt == mdan :: lhttp.cryptoval(this) :: lhttp.assignGuid(this).randomize(this.key) :: bin(this)
+:: mdan:decrypt == mdan :: lhttp.cryptoval(this) :: lhttp.renewGuid(this, this.key) :: return STTP
+
+// finish later \\
