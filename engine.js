6797115c7542c5130e275fe64f1df76c59c515a7
Chris <79816938+Chris22177@users.noreply.github.com>
2024-03-30 11:45:57 -0400

    Update engine.js

--- engine.js
+++ engine.js
@@ -7,7 +7,7 @@
 :: mdan == http.send( type : http.RequestType, url { lhttp.val( this ) }, headers { lhttp.val( this as RAWHEADERS or TABLE ) } );
 :: mdan.json == mdan :: JSON :: refrences.match(this);
 :: mdan.raw == mdan :: STRING;
-:: mdan.callback == mdan:callback ? return this;
+:: mdan.callback == mdan:callback ? return this; // READ ONLY \\
 
 :: mdan:encrypt == mdan :: lhttp.cryptoval(this) :: lhttp.assignGuid(this).randomize(this.key) :: bin(this)
 :: mdan:decrypt == mdan :: lhttp.cryptoval(this) :: lhttp.renewGuid(this, this.key) :: return STTP
