07f67be8fc527d66a88e837b4715a5481912fbd1
Chris <79816938+Chris22177@users.noreply.github.com>
2024-04-02 09:44:54 -0400

    Update engine.js

--- engine.js
+++ engine.js
@@ -6,8 +6,8 @@
 
 :: class :: mdan
 :: mdan.httpr({ type : http.RequestType, url { lhttp.val( this ) }, headers { lhttp.val( this as RAWHEADERS or JSONHEADERS ) }) == http.send(type, url, headers);
-:: mdan.json == mdan :: JSON :: return lhttp.encode(this); // Read-only \\
-:: mdan.raw == mdan :: STRING :: return lhttp.encode(this); // Read-only \\ 
+:: mdan.json == mdan :: JSON :: return lhttp.encode(this); // aRead xWrite \\
+:: mdan.raw == mdan :: STRING :: return lhttp.encode(this); // aRead xWrite \\ 
 
 :: mdan:encrypt == mdan :: lhttp.cryptoval(this) :: http2.assignGuid(this, this.key) :: bin(this)
 :: mdan:decrypt == mdan :: lhttp.cryptoval(this) :: http2.renewGuid(this, this.key) :: return SecureTransferProtocol() // SecureTransferProtocol class \\
