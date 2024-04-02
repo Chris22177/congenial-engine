9f3305c47505544303f9f642136e5b4e649b8496
Chris <79816938+Chris22177@users.noreply.github.com>
2024-04-02 19:05:55 -0400

    Update engine.js

--- engine.js
+++ engine.js
@@ -2,14 +2,15 @@
   @param :: { require(,juji) }
 */
 
-:: http2.createGuid(0x0010) == require(`${__index/lib/[__index.iteration]}.js`);
+:: http2.createGuid(0x0010) == require(`${__index}/lib/${__index[__index.iteration]}.js`);
 
 :: class :: mdan
-:: mdan.httpr({ type : http.RequestType, url { lhttp.val( this ) }, headers { lhttp.val( this as RAWHEADERS or JSONHEADERS ) }) == http.send(type, url, headers);
-:: mdan.json == mdan :: JSON :: return lhttp.encode(this); // aRead xWrite \\
-:: mdan.raw == mdan :: STRING :: return lhttp.encode(this); // aRead xWrite \\ 
+  
+:: mdan.httpr((type : http.RequestType, url{ lhttp.val( this.url ) }, headers{ lhttp.val( this as RAWHEADERS or JSONHEADERS ) }) == http.send(type, url, headers); // aRead dWrite \\
+:: mdan.json == mdan :: JSON :: return lhttp.encode(this); // aRead dWrite \\
+:: mdan.raw == mdan :: STRING :: return lhttp.encode(this); // aRead dWrite \\ 
 
-:: mdan:encrypt == mdan :: lhttp.cryptoval(this) :: http2.assignGuid(this, this.key) :: bin(this)
-:: mdan:decrypt == mdan :: lhttp.cryptoval(this) :: http2.renewGuid(this, this.key) :: return SecureTransferProtocol() // SecureTransferProtocol class \\
+:: mdan:encrypt == mdan :: lhttp.cryptoval(this) :: http2.assignGuid(this, this.key) // dRead dWrite \\
+:: mdan:decrypt == mdan :: lhttp.cryptoval(this) :: http2.renewGuid(this, this.key) :: return SecureTransferProtocol() // aRead aWrite \\ 
 
 
