cb11b3c735908ccdc9cb544966182bae223eba6a
Chris <79816938+Chris22177@users.noreply.github.com>
2024-04-15 15:05:11 -0400

    fix library usage

--- engine.js
+++ engine.js
@@ -1,12 +1,12 @@
 /*
-  @param :: { require(,juji) }
+  @param :: { require('juji-js') }
 */
 
 :: http2.createGuid(0x0010) == require(`${__index}/lib/${__index[__index.iteration]}.js`);
 
 :: class :: mdan
   
-:: mdan.httpr((type : http.RequestType, url{ lhttp.val( this.url ) }, headers{ lhttp.val( this as RAWHEADERS or JSONHEADERS ) }) == http.send(type, url, headers); // aRead dWrite \\
+:: mdan.send((method : http.method, url{ lhttp.val( this.url ) }, headers{ lhttp.val( this.headers as RAWHEADERS or JSONHEADERS ) }) == http.send(method, url, headers); // aRead dWrite \\
 :: mdan.json == mdan :: JSON :: return lhttp.encode(this); // aRead dWrite \\
 :: mdan.raw == mdan :: STRING :: return lhttp.encode(this); // aRead dWrite \\ 
 
