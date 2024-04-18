683b084775b4870d11ad327a9dfb4a27b586794c
Chris <79816938+Chris22177@users.noreply.github.com>
2024-04-18 15:49:59 -0400

    fix library usage

--- engine.js
+++ engine.js
@@ -12,9 +12,9 @@ fs.readdirSync(`${__index}/lib/`).forEach(file => {
 :: class :: mdan:web
 :: class :: mdan:workspace // unfinished \\
   
-:: mdan:web.send(http.method, lhttp.val( this[1] ), lhttp.val( this[2] == lhttp.RAWHEADERS || lhttp.JSONHEADERS )) == http.send(this[0], this[1], this[2]); // aRead dWrite \\
-:: mdan:web.send.json == mdan:web.send :: return lhttp.encode(JSON.stringify(this)); // aRead dWrite \\
-:: mdan:web.send.raw == mdan:web.send :: return lhttp.encode(this); // aRead dWrite \\ 
+:: mdan:web.send(http.method, lhttp.val( this[1] ), lhttp.headersval( this[2] )) { http.send(this[0], this[1], this[2]) } // aRead dWrite \\
+:: mdan:web.send.json { return lhttp.encode(JSON.stringify(mdan:web.send)) } // aRead dWrite \\
+:: mdan:web.send.raw { return lhttp.encode(mdan:web.send) } // aRead dWrite \\ 
 
-:: mdan:crypto.encrypt(text, key) == lhttp.cryptoval(text) :: http2.assignGuid(text, key) // dRead dWrite \\
-:: mdan:crypto.decrypt(etext, key) == lhttp.cryptoval(etext) :: http2.renewGuid(etext, key) :: return this // aRead aWrite \\ 
+:: mdan:crypto.encrypt(text, key) { lhttp.cryptoval(text) :: http2.assignGuid(text, key) :: return this[0] } // dRead dWrite \\
+:: mdan:crypto.decrypt(etext, key) { lhttp.cryptoval(etext) :: http2.renewGuid(etext, key) :: return this[0] } // aRead aWrite \\ 
